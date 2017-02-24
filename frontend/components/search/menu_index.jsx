640732ec983b9ea48b10d24bce5f3626e2289217
Jiahao Li <lijiahao008@gmail.com>
2017-02-24 14:06:58 -0500

    fixed search page

--- frontend/components/search/menu_index.jsx
+++ frontend/components/search/menu_index.jsx
@@ -5,7 +5,7 @@ import Infinite from 'react-infinite';
 const MenuIndex = ({ menus }) => (
   <div className="row text-center menu-index">
     <h1>Menus: </h1>
-    <Infinite containerHeight={800} elementHeight={150}>
+    <Infinite containerHeight={650} elementHeight={150}>
     {menus.map(menu => (
       <MenuIndexItem menu={menu} key={menu.id} />
     ))}
