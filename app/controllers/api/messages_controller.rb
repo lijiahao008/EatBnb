4f694107d54758a34d1b6fed8f753e80a8d6d465
Jiahao Li <lijiahao008@gmail.com>
2017-03-30 15:01:31 -0400

    removed debugger

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -5,7 +5,6 @@ class Api::MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     @conversation = current_user.send_message(recipients, params[:body], params[:subject]).conversation
-    debugger
     render 'api/conversations/conversation'
   end
 end
