8c4061f020e7532777199a0e21c9b4e0e9150d54
Jiahao Li <lijiahao008@gmail.com>
2017-04-07 02:00:38 -0400

    added new paperclip style

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ApplicationRecord
   validates :title, :price, :description, :address, :owner, presence:true
   before_save :downcase_address
 
-  has_attached_file :picture, default_url: "default-menu.jpg"
+  has_attached_file :picture, styles: { medium: "400x300>" }, default_url: "default-menu.jpg"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   after_validation :geocode
 
