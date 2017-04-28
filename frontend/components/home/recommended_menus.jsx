8654ecda12d840043a2c932195f4809ee78b622e
Jiahao Li <lijiahao008@gmail.com>
2017-04-27 20:25:29 -0400

    fixed menu-items detail css

--- frontend/components/home/recommended_menus.jsx
+++ frontend/components/home/recommended_menus.jsx
@@ -94,13 +94,14 @@ class RecommendedMenus extends React.Component {
              }]}>
              {
                this.props.menus.map(menu => (
-                 <div key={menu.id}>
+                 <div
+                   key={menu.id}>
                    <img src={menu.picture_url} alt={menu.title}
                    onClick={()=>hashHistory.push(`/menus/${menu.id}`)}
                    height= "200"
                    width="260"
                    />
-                   <div className="home-menu-items menu-items-detail">
+                 <div className="home-menu-items menu-items-detail">
                      <div className="menu-items-price">
                       ${menu.price}  {menu.title}
                      </div>
